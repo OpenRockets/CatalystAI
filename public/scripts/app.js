c750d9cb52dee0d77428febde3008a9e15c9e401
Neksha DeSilva <nekshavs@gmail.com>
2025-04-14 06:47:53 +0530

    Update02

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,6 @@
+/*Copyrights (c) 2025 NekshaDeSilva and all other contributors in CatalystAI team.
+Originally created for CodeJAM 2025 Comp., Copyrights (c) University of Moratuwa, Sri Lanka.
+Licenced with Apache-2.0 contributor's license*/
+/*https://www.github.com/nekshadesilva/catalystai*/
+
 console.log('WORKING')
