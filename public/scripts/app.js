e7ebc69d2172d179d1fbde23ef2af0a8a2fbcb5c
Neksha DeSilva <nekshavs@gmail.com>
2025-04-20 14:54:59 +0530

    Update 07

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function(){
 .then((response)=>{
     console.log(response);
 })
-.error((error)=>{
+.catch((error)=>{
     console.log(error);
 })
 })
@@ -92,7 +92,10 @@ function randomeIdeaGen(){
                 function readFrr(){
                     return Math.floor(Math.random()*15) + 16;
                 }
-                
+                if (!text || typeof text !== 'string') {
+                  console.warn("writeit: text is undefined or not a string");
+                  return;
+              }
                 
                     if (index < text.length) {
                         if(21>x){
@@ -150,19 +153,13 @@ $('.buttonDiv_cvx-cont').on('click', function () {
 //There should be a mode called "BookMode" right beneath chat bubbles to search for internet and think function
 
 // The colour of the filecontmeasurerr_func_box the icon should be changed as the media inserted to. PDF-red, Image - purple rtc.
-function verctorReburnUI(){
-    $('.verctorReburnUI').on('click', function(){
-    
-        $(this).toggleClass('backdeggft_catalystaiUI');
-        setTimeout(()=>{
-            $(this).removeClass('backdeggft_catalystaiUI');
-        },100)
-    
-    
+$('body').on('click', '.verctorReburnUI', function () {
+  $(this).toggleClass('backdeggft_catalystaiUI');
+  setTimeout(() => {
+      $(this).removeClass('backdeggft_catalystaiUI');
+  }, 100);
+});
 
-})
-}
-verctorReburnUI();
 
 var u76376Status = 0;
 function TypingGesturesAdd(div) {
@@ -170,7 +167,7 @@ function TypingGesturesAdd(div) {
     if(u76376Status ==1){
 
     
-    $(div).parent().find('div').css({
+    $(div).parent().find('.jfjYrtUJYFSRFrANDOMdivcATALYST').css({
         'opacity':'100%',
         'position':'relative',
         'display':'inline-block'
@@ -178,7 +175,7 @@ function TypingGesturesAdd(div) {
     let index = 0;
     const text = $(div).html(); 
     $(div).html(''); 
-
+    console.log(div)
     function readFrr() {
         return Math.floor(Math.random() * 21) + 20; 
     }
@@ -192,7 +189,7 @@ function TypingGesturesAdd(div) {
             index++;
             setTimeout(innerFunc, readFrr());
         } else {
-            $(div).parent().find('div').css({
+            $(div).parent().find('.jfjYrtUJYFSRFrANDOMdivcATALYST').css({
                 'opacity':'0%',
                 'position':'fixed'
             });
@@ -471,7 +468,7 @@ const buttonSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 buttonSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
 buttonSvg.setAttribute('viewBox', '0 0 16 16');
 const globePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-globePath.setAttribute('d', 'M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z');
+globePath.setAttribute('d', 'M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0 3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z');
 buttonSvg.appendChild(globePath);
 button.appendChild(buttonSvg);
 
@@ -485,6 +482,113 @@ bubble.appendChild(postDiv);
 chatContent.appendChild(bubble);
 //end-credit-functions
 
-verctorReburnUI();
 
 }
+var intensityChatResponse = 100;
+var recurrentVariable_env_lopingPar =0;
+function dynamicChatBubble_handelr_chatBotQ(nativeOffer_dynamicrecurrentRetnOut, currentstateX ) {
+  recurrentVariable_env_lopingPar++
+  const chatContent = document.querySelector('.chatContent');
+  const chatContentInnerCenDiv  = document.createElement('div');
+  chatContentInnerCenDiv.className = 'chatContentCVX_block';
+
+  const chatBubble = document.createElement('div');
+  chatBubble.className = 'chatBubble_payLoad-ATT';
+
+  const boxCurrentState = document.createElement('div');
+  boxCurrentState.className = 'boxCurrentState_chatbubble';
+
+  const stateDiv = document.createElement('div');
+  const stateText = document.createElement('span');
+  stateText.textContent = currentstateX;
+
+  stateDiv.appendChild(stateText);
+  boxCurrentState.appendChild(stateDiv);
+  chatBubble.appendChild(boxCurrentState);
+
+
+  const chatContentDiv = document.createElement('div');
+  chatContentDiv.className = `recurrent_ChatContent_bubble HTPS_div-bot-chat-content-fxd${recurrentVariable_env_lopingPar}`;
+  chatContentDiv.setAttribute('onclick', "TypingGesturesAdd(div='.HTPS_div-bot-chat-content-fxd span')");
+
+  const messageSpan = document.createElement('span');
+
+  
+  messageSpan.innerHTML = nativeOffer_dynamicrecurrentRetnOut;
+  chatContentDiv.appendChild(messageSpan);
+  chatBubble.appendChild(chatContentDiv);
+
+  // Delay the TypingGesturesAdd call to ensure the element is fully rendered
+  setTimeout(() => {
+      const selector = `.HTPS_div-bot-chat-content-fxd${recurrentVariable_env_lopingPar} span`;
+      const targetElement = document.querySelector(selector);
+
+      if (targetElement) {
+          TypingGesturesAdd(selector);
+          console.log('TypingGesturesAdd triggered for:', targetElement);
+      } else {
+          console.error('Target element not found:', selector);
+      }
+  }, 0); // Delay execution to the next event loop cycle
+
+  const additionalDiv = document.createElement('div');
+  additionalDiv.className = 'jfjYrtUJYFSRFrANDOMdivcATALYST';
+  chatContentDiv.appendChild(additionalDiv);
+
+  const endSlider = document.createElement('div');
+  endSlider.className = 'endSlider_opper-buttons_chatbubble';
+
+  const deepThinkButton = document.createElement('div');
+  deepThinkButton.className = 'chatBubble-button chatBubble-button-deepthink verctorReburnUI';
+  deepThinkButton.setAttribute('data-massIntensityThis', intensityChatResponse);
+
+  //Us getAttribute() for retieval.
+  const deepThinkSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  deepThinkSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  deepThinkSvg.setAttribute('width', '16');
+  deepThinkSvg.setAttribute('height', '16');
+  deepThinkSvg.setAttribute('fill', 'currentColor');
+  deepThinkSvg.classList.add('bi', 'bi-stars');
+  deepThinkSvg.setAttribute('viewBox', '0 0 16 16');
+
+  const deepThinkPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  deepThinkPath.setAttribute('d', 'M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z');
+  deepThinkSvg.appendChild(deepThinkPath);
+  deepThinkButton.appendChild(deepThinkSvg);
+
+  const deepThinkText = document.createElement('span');
+  deepThinkText.textContent = 'Deep Think';
+  deepThinkButton.appendChild(deepThinkText);
+
+  endSlider.appendChild(deepThinkButton);
+
+
+  const webSearchButton = document.createElement('div');
+  webSearchButton.className = 'chatBubble-button chatBubble-button-searchWeb verctorReburnUI';
+
+  const webSearchSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  webSearchSvg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  webSearchSvg.setAttribute('viewBox', '0 0 16 16');
+
+  const webSearchPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  webSearchPath.setAttribute('d', 'M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0 3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z');
+  webSearchSvg.appendChild(webSearchPath);
+  webSearchButton.appendChild(webSearchSvg);
+
+  const webSearchText = document.createElement('span');
+  
+  webSearchText.textContent = 'Web Search';
+  webSearchButton.appendChild(webSearchText)
+  endSlider.appendChild(webSearchButton);
+
+  chatBubble.appendChild(endSlider);
+
+  chatContentInnerCenDiv.appendChild(chatBubble);
+  chatContent.appendChild(chatContentInnerCenDiv);
+
+}
+$('.chatBubble-button-deepthink').on('click', function(attr_intensity_reverted, textContextV){
+  attr_intensity_reverted = $(this).attr('data-massIntensityThis');
+  textContextV  = $(this).parent().parent().find('.recurrent_ChatContent_bubble span').html();
+  console.log(attr_intensity_reverted , textContextV)
+})
